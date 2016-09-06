2ecc95c312b95160b8c727c31fcfef8ac16347c5
TheEjoshua <e.patanao@gmail.com>
2016-09-06 15:44:33 +0800

    BAGO

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/contact'
+
   devise_for :users
-  get 'welcome/index'
-  root 'welcome#index'
+  #get 'welcome/index'
+  root 'page#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
